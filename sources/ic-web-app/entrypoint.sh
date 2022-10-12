ff470d44c271c94d97a11555ee28d6d0cbf2afec
Emmanuel Girin <66262049+EmmanuelGirin@users.noreply.github.com>
2022-10-12 12:58:31 +0200

    changed directory structure

---
+++ sources/ic-web-app/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ODOO_URL=$(awk '/ODOO/ {sub(/^.* *ODOO/,""); print $2}' ../releases.txt)
+export PGADMIN_URL=$(awk '/PGADMIN/ {sub(/^.* *PGADMIN/,""); print $2}' ../releases.txt)
+python app.py
